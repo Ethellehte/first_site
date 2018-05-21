f68970edbc2ff5f4f262defdcf3ba777830f9523
Yuriy <last1992@list.ru>
2018-05-21 11:04:20 +0300

    Add third page

---
+++ first_site/js/song.js
@@ -0,0 +1,103 @@
+var $mybutton = document.getElementById('my_btn')
+$mybutton.onclick = function() {
+	var myaudio = document.getElementById('my_audio');
+	if (myaudio.paused == true) {
+		 document.getElementById('my_audio').play()
+		 this.style.backgroundColor = "red"
+		 this.style.color = "white"
+	} else if (myaudio.paused == false ) {
+		document.getElementById('my_audio').pause()
+			this.style.backgroundColor = "white"
+			this.style.color = "black"
+	}
+}
+var $mybutton2 = document.getElementById('my_btn2')
+$mybutton2.onclick = function() {
+	var myaudio2 = document.getElementById('my_audio2');
+	if (myaudio2.paused == true) {
+		 document.getElementById('my_audio2').play()
+		 this.style.backgroundColor = "red"
+		 this.style.color = "white"
+	} else if (myaudio2.paused == false ) {
+		document.getElementById('my_audio2').pause()
+			this.style.backgroundColor = "white"
+			this.style.color = "black"
+	}
+}
+var $mybutton3 = document.getElementById('my_btn3')
+$mybutton3.onclick = function() {
+	var myaudio = document.getElementById('my_audio3');
+	if (myaudio.paused == true) {
+		 document.getElementById('my_audio3').play()
+		 this.style.backgroundColor = "red"
+		 this.style.color = "white"
+	} else if (myaudio.paused == false ) {
+		document.getElementById('my_audio3').pause()
+			this.style.backgroundColor = "white"
+			this.style.color = "black"
+	}
+}
+var $mybutton4 = document.getElementById('my_btn4')
+$mybutton4.onclick = function() {
+	var myaudio = document.getElementById('my_audio4');
+	if (myaudio.paused == true) {
+		 document.getElementById('my_audio4').play()
+		 this.style.backgroundColor = "red"
+		 this.style.color = "white"
+	} else if (myaudio.paused == false ) {
+		document.getElementById('my_audio4').pause()
+			this.style.backgroundColor = "white"
+			this.style.color = "black"
+	}
+}
+var $mybutton5 = document.getElementById('my_btn5')
+$mybutton5.onclick = function() {
+	var myaudio = document.getElementById('my_audio5');
+	if (myaudio.paused == true) {
+		 document.getElementById('my_audio5').play()
+		 this.style.backgroundColor = "red"
+		 this.style.color = "white"
+	} else if (myaudio.paused == false ) {
+		document.getElementById('my_audio5').pause()
+			this.style.backgroundColor = "white"
+			this.style.color = "black"
+	}
+}
+
+var $mybutton6 = document.getElementById('my_btn6')
+$mybutton6.onclick = function() {
+	var myaudio = document.getElementById('my_audio6');
+	if (myaudio.paused == true) {
+		 document.getElementById('my_audio6').play()
+		 this.style.backgroundColor = "red"
+		 this.style.color = "white"
+	} else if (myaudio.paused == false ) {
+		document.getElementById('my_audio6').pause()
+			this.style.backgroundColor = "white"
+			this.style.color = "black"
+	}
+}
+
+var $mybutton7 = document.getElementById('my_btn7')
+$mybutton7.onclick = function() {
+	var myaudio = document.getElementById('my_audio7');
+	if (myaudio.paused == true) {
+		 document.getElementById('my_audio7').play()
+		 this.style.backgroundColor = "red"
+		 this.style.color = "white"
+	} else if (myaudio.paused == false ) {
+		document.getElementById('my_audio7').pause()
+			this.style.backgroundColor = "white"
+			this.style.color = "black"
+	}
+}
+
+
+
+
+
+
+
+
+
+
